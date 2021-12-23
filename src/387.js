1e4e37918207dfc4518ce97765e08ea11d11d0fb
suhendra.utet <suhendra.utet@supplylogix.io>
2021-12-23 17:18:20 -0600

    problem 387

---
+++ src/387.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    //let uniq = new Set();
+    for (let i = 0; i < s.length; i++) {
+        //if (!uniq.has(s[i]))
+        //    uniq.add(s[i]);
+
+        let sub = s[i], isUniq = true;
+        let newS = s.substring(0, i) + s.substring(i + 1, s.length);
+        //console.log(newS);
+
+        for (let j = 0; j < newS.length; j++) {
+            if (sub == newS[j]) {
+                //console.log(newS[j]);
+                isUniq = false;
+                break;
+            }
+        }
+
+        if (isUniq)
+            return i;
+    }
+
+    //console.log(uniq);
+};
+
+let str = "leetcode"; // answer is 0
+//let str = "loveleetcode"; // answer is 2
+let res = firstUniqChar(str);
+console.log(res);
