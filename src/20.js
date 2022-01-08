d3672267dbee2edc1e66a63c19308488c764f163
Suhendra Utet <suhendra.utet@supplylogix.io>
2022-01-08 17:17:50 -0600

    solution 20

--- src/20.js
+++ src/20.js
@@ -3,6 +3,29 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    let q = [], openBracketsIdx = ['(', '{', '['], closedBracketsIdx = [')', '}', ']'], openBrackets = new Set(openBracketsIdx), closedBrackets = new Set(closedBracketsIdx);
+    for (let i = 0; i < s.length; i++) {
+        let l = s[i];
+        if (openBrackets.has(l))
+            q.push(l);
+        if (closedBrackets.has(l))
+        {
+            if (q.length == 0)
+                return false;
+
+            let ob = q.pop();
+            if (closedBracketsIdx.indexOf(l) != openBracketsIdx.indexOf(ob))
+                return false;
+        }
+    }
+
+    if (q.length != 0)
+        return false;
+    
+    return true;
+
+    // Attempt 1: using math count.
+    /*
     let counter = 0, openBrackets = new Set(['(', '{', '[']), closedBrackets = new Set([')', '}', ']']);
     for (let i = 0; i < s.length; i++) {
         if (openBrackets.has(s[i]))
@@ -13,6 +36,7 @@ var isValid = function(s) {
     if (counter != 0)
         return false;
     return true;
+    */
 };
 
 //https://leetcode.com/problems/valid-parentheses/
