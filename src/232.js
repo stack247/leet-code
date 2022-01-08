80950414beb5e0732d6f7f315784fdfbddd8908b
Suhendra Utet <suhendra.utet@supplylogix.io>
2022-01-08 17:53:36 -0600

    solution 232

--- src/232.js
+++ src/232.js
@@ -1,6 +1,10 @@
 
 var MyQueue = function() {
-    
+    this.s1 = [];
+    this.s2 = [];
+    // a = ['1'];
+    //console.log(a);
+    //console.log(a.peek);
 };
 
 /** 
@@ -8,28 +12,45 @@ var MyQueue = function() {
  * @return {void}
  */
 MyQueue.prototype.push = function(x) {
-    
+    //console.log(this.s1);
+    this.s1.push(x);
 };
 
 /**
  * @return {number}
  */
 MyQueue.prototype.pop = function() {
-    
+    //console.log(this.s1);
+    if (this.s2.length == 0)
+    {
+        //console.log(this.s1);
+        //console.log(this.s2);
+        while (this.s1.length > 0) {
+            let s1p = this.s1.pop();
+            //console.log(s1p);
+            this.s2.push(s1p);
+        }
+        //console.log(this.s2);
+    }
+    return this.s2.pop();
 };
 
 /**
  * @return {number}
  */
 MyQueue.prototype.peek = function() {
-    
+    //console.log(this.s1);
+    //return this.s1.peek;
+    if (this.s2.length > 0)
+        return this.s2[this.s2.length - 1];
+    return this.s1[0];
 };
 
 /**
  * @return {boolean}
  */
 MyQueue.prototype.empty = function() {
-    
+    return this.s1.length == 0 && this.s2.length == 0;
 };
 
 /** 
@@ -41,12 +62,16 @@ MyQueue.prototype.empty = function() {
  * var param_4 = obj.empty()
  */
 
-https://leetcode.com/problems/implement-queue-using-stacks/
-let str = "()"; // answer is true
-//let str = "()[]{}"; // answer is true
-//let str = "(]"; // answer is true
-let res = isValid(str);
+// https://leetcode.com/problems/implement-queue-using-stacks/
+var obj = new MyQueue();
+obj.push(1);
+obj.push(2);
+var param_2 = obj.peek();
+var param_3 = obj.pop();
+var param_4 = obj.empty();
 
 console.log('-------------------');
-console.log('parameter ' + str);
-console.log(res);
+console.log('obj ' + obj);
+console.log('param_2 ' + param_2);
+console.log('param_3 ' + param_3);
+console.log('param_4 ' + param_4);
