e1d0f6a340e8f9b2dc8a7e4523689f1ad8e43351
Suhendra Utet <suhendra.utet@supplylogix.io>
2022-01-09 13:36:13 -0600

    template 94

---
+++ src/94.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ var inorderTraversal = function(root) {
+    
+};
+
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+
+https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+let res = inorderTraversal(rootObj);
+
+console.log('-------------------');
+console.log('root ' + root);
+//console.log(rootObj.val);
+//console.log(rootObj.left.val);
+//console.log(rootObj.right.val);
+//console.log(rootObj.left.left.val);
+//console.log(rootObj.left.right.val);
+//console.log(rootObj.right.left.val);
+//console.log(rootObj.right.right.val);
+console.log('res ' + res);
