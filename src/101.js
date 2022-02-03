bd753c378970f51a8f44c45eaccbc2c1d34f9741
suhendra.utet <suhendra.utet@supplylogix.io>
2022-02-03 11:15:56 -0600

    try with index js

--- src/101.js
+++ src/101.js
@@ -13,8 +13,9 @@
 var isSymmetric = function(root) {
     console.log(root.val);
     //console.log(root.left.val);
-    //console.log(root.right.val);
-    return false;
+    console.log(root.right.val);
+    //return false;
+    return true;
 };
 
 function TreeNode(val, left, right) {
