bfc00e6b4868341ccfc235ef1d451d789724f392
stack247 <stack247>
2022-02-03 23:49:29 -0600

    solution 101

--- src/101.js
+++ src/101.js
@@ -11,11 +11,41 @@
  * @return {boolean}
  */
 var isSymmetric = function(root) {
-    console.log(root.val);
-    //console.log(root.left.val);
-    console.log(root.right.val);
-    //return false;
-    return true;
+    let res=false, lq=[], rq=[];
+    
+    if (root.left)
+        lq.push(root.left);
+    if (root.right)
+        rq.push(root.right);
+    if (lq.length !== rq.length)
+        return res;
+    
+    while (lq.length > 0 || rq.length > 0) {
+        let l = lq.pop();
+        let r = rq.pop();
+        //console.log(l.val);
+        //console.log(r.val);
+        if ((!l && r ) || (l && !r) || (l && r && l.val && r.val && l.val !== r.val)) {
+            //console.log(l);
+            //console.log(r);
+            return res;
+        }
+        
+        if (l) {
+            lq.push(l.left);
+            lq.push(l.right);
+        }
+        if (r) {
+            rq.push(r.right);
+            rq.push(r.left);
+        }
+        
+        //if (lq.length !== rq.length)
+        //    return res;
+    }
+    
+    res = true;
+    return res;
 };
 
 function TreeNode(val, left, right) {
@@ -25,12 +55,12 @@ function TreeNode(val, left, right) {
 }
 
 // https://leetcode.com/problems/symmetric-tree/
-let rootObj = new TreeNode(1, new TreeNode(2, new TreeNode(3, null, null), new TreeNode(4, null, null)), new TreeNode(2, new TreeNode(3, null, null), new TreeNode(4, null, null))); // answer is true
+let rootObj = new TreeNode(1, new TreeNode(2, new TreeNode(3, null, null), new TreeNode(4, null, null)), new TreeNode(2, new TreeNode(4, null, null), new TreeNode(3, null, null))); // answer is true
 //let rootObj = new TreeNode(1, new TreeNode(2, null, new TreeNode(3, null, null)), new TreeNode(2, null, new TreeNode(3, null, null))); // answer is false
 let res = isSymmetric(rootObj);
 
 console.log('-------------------');
-console.log(rootObj.val);
+//console.log(rootObj.val);
 //console.log(rootObj.left.val);
 //console.log(rootObj.right.val);
 //console.log(rootObj.left.left.val);
