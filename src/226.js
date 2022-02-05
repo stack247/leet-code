af14d9101b073bc8df8f3f6fc4318d4ae8e2be08
Suhendra Utet <suhendra.utet@supplylogix.io>
2022-02-05 14:16:53 -0600

    solution 226

--- src/226.js
+++ src/226.js
@@ -11,7 +11,48 @@
  * @return {TreeNode}
  */
 var invertTree = function(root) {
+    if (root == null)
+        return root;
+
+    let q=[root];
+
+    while (q.length) {
+        let c = q.pop();
+        let l = c.left;
+        let r = c.right;
+        //console.log(l);
+        //console.log(r);
+        //console.log(c.right);
+        c.right = l;
+        if (l)
+            q.push(c.right);
+        c.left = r;
+        if (r)
+            q.push(c.left);
+
+        //console.log(c.left);
+        //console.log(c.right);
+        //console.log('--------------');
+    }
+
+    return root;
     
+    /* Attempt 1: use left and right queue.
+    let r=root,lq=[],rq=[];
+
+    if (root.left)
+        rq.push(root.left);
+    if (root.right)
+        lq.push(root.right);
+
+    while (rq.length || lq.length) {
+        r.left = lq.pop();
+        r.right = rq.pop();
+
+    }
+
+    return root;
+    */
 };
 
 function TreeNode(val, left, right) {
@@ -21,7 +62,10 @@ function TreeNode(val, left, right) {
 }
 
 // https://leetcode.com/problems/invert-binary-tree/
-let rootObj = new TreeNode(2, new TreeNode(1, null, null), new TreeNode(3, null, null)); // answer is [2,3,1]
+//let rootObj = new TreeNode(null, null, null); // answer is []
+let rootObj = new TreeNode(4, new TreeNode(2, new TreeNode(1, null, null), new TreeNode(3, null, null)), new TreeNode(7, new TreeNode(6, null, null), new TreeNode(9, null, null))); // answer is [4,7,2,9,6,3,1]
+//let rootObj = new TreeNode(2, new TreeNode(1, null, null), new TreeNode(3, null, null)); // answer is [2,3,1]
+//let rootObj = new TreeNode(1, new TreeNode(2, null, null), null); // answer is [1,null,2]
 let res = invertTree(rootObj);
 
 console.log('-------------------');
@@ -32,4 +76,5 @@ console.log('-------------------');
 //console.log(rootObj.left.right.val);
 //console.log(rootObj.right.left.val);
 //console.log(rootObj.right.right.val);
-console.log('res ' + res);
+console.log('res');
+console.log(res);
