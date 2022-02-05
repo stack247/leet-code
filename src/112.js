3971b9a82ae2accb33154c206c2bc711af78b5dd
Suhendra Utet <suhendra.utet@supplylogix.io>
2022-02-05 15:22:34 -0600

    attemp 1 112.

--- src/112.js
+++ src/112.js
@@ -12,7 +12,27 @@
  * @return {boolean}
  */
 var hasPathSum = function(root, targetSum) {
-    
+    // Attempt 1: depth-first traversal for each possible path
+    let q=[],c=root,r=false,beforeLeafParent;
+    while (c) {
+        q.push(c);
+
+        // Leaf
+        if (!c.left && !c.right) {
+            // Do the sum
+
+            // If not the right path, go back up to parent
+            c = beforeLeafParent;
+        }
+        
+        // Always assume next node is leaf.
+        beforeLeafParent = c;
+        if (c.left);
+            c = c.left;
+        if (c.right);
+            c = c.right;
+    }
+    return r;
 };
 
 function TreeNode(val, left, right) {
@@ -23,7 +43,9 @@ function TreeNode(val, left, right) {
 
 // https://leetcode.com/problems/path-sum/
 let rootObj = new TreeNode(5, new TreeNode(4, new TreeNode(11, new TreeNode(7, null, null), new TreeNode(2, null, null)), null), new TreeNode(8, new TreeNode(13, null, null), new TreeNode(4, null, new TreeNode(1, null, null)))); // answer is true
-let targetSum = 5;
+let targetSum = 22;
+//let rootObj = new TreeNode(1, new TreeNode(2, null, null), new TreeNode(3, null, null)); // answer is false
+//let targetSum = 5;
 let res = hasPathSum(rootObj, targetSum);
 
 console.log('-------------------');
